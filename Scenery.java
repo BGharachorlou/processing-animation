ed3b1763ef7fd5ff04e42fb6e3533d53be1cfeb0
BGharachorlou <36392796+BGharachorlou@users.noreply.github.com>
2018-04-25 11:06:30 -0400

    Create Scenery.java

---
+++ Scenery.java
@@ -0,0 +1,33 @@
+void setup() {
+  size(664,350);
+
+noStroke();
+  fill(#35FF69);
+  rect(0, 0, 664, 350);
+
+noStroke();
+  fill(#1982C4);
+  rect(0, 0, 664, 234);
+  
+noStroke();
+  fill(#3B9158);
+  ellipse(150, 200, 20, 20);
+
+noStroke();
+  fill(#3B9158);
+  ellipse(170, 200, 20, 20);
+  
+noStroke();
+  fill(#3B9158);
+  ellipse(160, 200, 20, 20);
+  
+noStroke();
+  fill(#3B9158);
+  ellipse(160, 190, 20, 20);
+  
+noStroke();
+  fill(#684626);
+  rect(155, 210, 10, 25);
+  
+
+}
