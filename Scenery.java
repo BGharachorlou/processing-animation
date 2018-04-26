f9bb25eebfba1e98131d3ce44670c47903876671
BGharachorlou <36392796+BGharachorlou@users.noreply.github.com>
2018-04-26 11:09:25 -0400

    Update Scenery.java

--- Scenery.java
+++ Scenery.java
@@ -1,6 +1,17 @@
+PVector[] clouds;
+
 void setup() {
   size(664,350);
-
+  
+  clouds = new PVector[10];
+  for (int i = 0; i < clouds.length; i++) {
+    float x = random(0, width);
+    float y = random(0, 117);
+    clouds[i] = new PVector(x, y);
+  }
+}
+void draw() {
+  
 noStroke();
   fill(#35FF69);
   rect(0, 0, 664, 350);
@@ -25,9 +36,24 @@ noStroke();
   fill(#3B9158);
   ellipse(160, 190, 20, 20);
   
+for (int i = 0; i < clouds.length; i++) {
+    // move each cloud
+    clouds[i].add(1, 0);
+    
+    // reset if they go too far
+    if (clouds[i].x - 75 > width) {
+      clouds[i].x = -75;
+      clouds[i].y = (int) random(50, 117);
+    }
+  
 noStroke();
   fill(#684626);
   rect(155, 210, 10, 25);
-  
 
+noStroke();
+  fill(255);
+  ellipse(clouds[i].x, clouds[i].y, 40, 30);
+  ellipse(clouds[i].x+10, clouds[i].y+15, 40, 30);
+  ellipse(clouds[i].x-10, clouds[i].y+15, 40, 30);
+  }
 }
